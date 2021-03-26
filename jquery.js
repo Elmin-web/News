26c84f636cc5d696306ed10780f33f2f68f2ea37
Elmin <elmin555@bk.ru>
2021-03-26 15:49:11 +0400

    change Jquery

--- jquery.js
+++ jquery.js
@@ -1,3 +1,10 @@
 $(".profile .profile-name").on("click", (e) => {
+  e.stopPropagation();
   $(".dropdown-content").slideToggle(200);
+});
+$(".dropdown-content").on("click", (e) => {
+  e.stopPropagation();
+});
+$(document).on("click", (e) => {
+  $(".dropdown-content").slideUp(200);
 });
